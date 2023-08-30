79286d98bdb122f64300ff0a4c4e9f2c4f12d143
gadgul-aishwarya <aishwaryagadgul@github.com>
2023-08-30 22:20:32 +0530

    added

---
+++ calculator.js
@@ -0,0 +1,14 @@
+// Created by Aishwarya
+
+
+
+alert('HELLO ALL)
+
+$(document).ready(function(){
+    $(".vibrate").click(function(){
+        window.navigator.vibrate(50);
+     
+    });
+});
+
+
